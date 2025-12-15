90776e1c8ea2e23cd4e9404436aa2f779c1ec01d
ShelpAm <shelpam@outlook.com>
2025-12-15 19:13:11 +0800

    fix: home() fails on windows

--- include/hc/xdg-basedir.h
+++ include/hc/xdg-basedir.h
@@ -17,22 +17,29 @@ namespace fs = std::filesystem;
 
 inline fs::path home()
 {
-    auto homedir = env("HOME");
-    assert(homedir.has_value());
-    return homedir.value();
+    // POSIX
+    if (auto p = env("HOME"); p)
+        return *p;
+
+#ifdef _WIN32
+    if (auto p = env("USERPROFILE"); p)
+        return *p;
+#endif
+
+    throw std::runtime_error{"Cannot determine home directory: no appropriate "
+                             "environment variable presents"};
 }
 
 inline fs::path config_home()
 {
     auto dir = env("XDG_CONFIG_HOME");
-    return dir.has_value() ? fs::path{dir.value()} : home() / ".config";
+    return dir.has_value() ? fs::path{*dir} : home() / ".config";
 }
 
 inline fs::path data_home()
 {
     auto dir = env("XDG_DATA_HOME");
-    return dir.has_value() ? fs::path{dir.value()}
-                           : home() / ".local" / "share";
+    return dir.has_value() ? fs::path{*dir} : home() / ".local" / "share";
 }
 
 } // namespace xdg
